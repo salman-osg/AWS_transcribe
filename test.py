16cc52076d27769b03c0a46205aa3fe65517d5a8
khan096 <knowsallu@gmail.com>
2022-08-16 14:03:25 +0530

    AWS Commit

---
+++ test.py
@@ -0,0 +1,43 @@
+import boto3
+import time
+import urllib
+import json
+    
+transcribe_client = boto3.client('transcribe')
+
+def transcribe_file(job_name, file_uri, transcribe_client):
+    transcribe_client.start_transcription_job(
+        TranscriptionJobName=job_name,
+        Media={'MediaFileUri': file_uri},
+        MediaFormat='mp3',
+        IdentifyMultipleLanguages = True,
+    LanguageOptions = [
+        'en-US', 'hi-IN', 'fr-FR'])
+
+    max_tries = 60
+    while max_tries > 0:
+        max_tries -= 1
+        job = transcribe_client.get_transcription_job(TranscriptionJobName=job_name)
+        job_status = job['TranscriptionJob']['TranscriptionJobStatus']
+        if job_status in ['COMPLETED', 'FAILED']:
+            print(f"Job {job_name} is {job_status}.")
+            if job_status == 'COMPLETED':
+                response = urllib.request.urlopen(job['TranscriptionJob']['Transcript']['TranscriptFileUri'])
+                data = json.loads(response.read())
+                text = data['results']['transcripts'][0]['transcript']
+                print("========== below is output of speech-to-text ========================")
+                print(text)
+                print("=====================================================================")
+            break
+        else:
+            print(f"Waiting for {job_name}. Current status is {job_status}.")
+        time.sleep(10)
+
+
+def main():
+    file_uri = 's3://speech-to-text-test2/test1_dhruv.mp3'
+    transcribe_file('Example-job2', file_uri, transcribe_client)
+
+
+if __name__ == '__main__':
+    main()
